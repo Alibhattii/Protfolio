1c4a67760367dfc31aa4155086dc13078531de6c
Alibhattii <m.abforguestpost@gmail.com>
2024-09-27 15:25:02 -0700

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,7 @@
+var multiple = new Typed (".multiple" ,{
+	strings:["Fill The Form"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop:true 
+})
---
+++ script.js
@@ -0,0 +1,7 @@
+var multiple = new Typed (".multiple" ,{
+	strings:["Bloger" ,"Fronted Developer"  , "Gamer" , "Graphic Design"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop:true 
+})
